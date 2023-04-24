dca41dde795424fdc6309e876542ace07311f18a
tomcampfens <tomcampfens97@gmail.com>
2023-04-24 17:11:10 +0200

    json commit

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
-
+# %%
+from flask import Flask, jsonify
+import pandas as pd
 app = Flask(__name__)
-
+#df = pd.read_csv("recipe_voorbeeld.csv")
 @app.route("/")
 def read_csv():
-    
-    return "<p>Hello, World!</p>"
+    return '<table><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr></table>'
+
+@app.route("/recipes")
+def recipes():
+    with open('recipe_voorbeeld.csv') as csvfile:
+        csvreader = pd.read_csv(csvfile)
+        data = []
+        for row in csvreader:
+            data.append(row)
+        return jsonify(data)
+    #return '<table><tr><td>{df[1]}</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr></table>'
+
+# %%
--- readrecipes.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("pokemon.csv")
-print(df.to_string())
