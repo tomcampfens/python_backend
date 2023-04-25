008499863ab6752b16663ac1bc4bf5393e009690
tomcampfens <tomcampfens97@gmail.com>
2023-04-25 11:52:11 +0200

    herstellen

---
+++ my_database.py
@@ -0,0 +1,28 @@
+import mysql.connector
+import pandas as pd
+with open('recipe_voorbeeld.csv') as csvfile:
+    csvreader = pd.read_csv(csvfile)
+    df = pd.DataFrame(csvreader)
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='grillfest',
+                                         user='root',
+                                         password='')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select * from ingredients")
+        record = cursor.fetchall()
+        for x in record:
+            print(x)
+        print("You're connected to database: ", record)
+
+except mysql.connector.Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    print("MySQL connection is closed")
+
+cursor.close()
+connection.close()
