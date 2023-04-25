21d91643febe7a325184e260582d64761e52c3b5
tomcampfens <tomcampfens97@gmail.com>
2023-04-25 10:51:27 +0200

    werkende code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # %%
-from flask import Flask, jsonify
+from flask import Flask
 import pandas as pd
 app = Flask(__name__)
 #df = pd.read_csv("recipe_voorbeeld.csv")
@@ -7,14 +7,19 @@ app = Flask(__name__)
 def read_csv():
     return '<table><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr></table>'
 
+csvreader = pd.read_csv('recipe_voorbeeld.csv')
+df = pd.DataFrame(csvreader)
+df.to_json()
+##data = []
+#for row in csvreader:
+#    data.append(row)
+
 @app.route("/recipes")
 def recipes():
     with open('recipe_voorbeeld.csv') as csvfile:
         csvreader = pd.read_csv(csvfile)
-        data = []
-        for row in csvreader:
-            data.append(row)
-        return jsonify(data)
+        df = pd.DataFrame(csvreader)
+        return df.to_json()
     #return '<table><tr><td>{df[1]}</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr></table>'
 
 # %%
