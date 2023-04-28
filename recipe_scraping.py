5a6c8ca95bb18e318b4739044234e8987832fb84
yasmine-am <yasmine.amadou1997@gmail.com>
2023-04-28 16:33:48 +0200

    Scrapping

--- recipe_scraping.py
+++ recipe_scraping.py
@@ -1,20 +1,41 @@
 import requests, csv
 from bs4 import BeautifulSoup
 
-url = 'https://github.com/cweber/cookbook/blob/master/recipes.csv'
+def getInfoVanDetail(nwurl):
+    print("we gaan deze beoeken", nwurl)
 
-with open('Recipe.csv', "w", encoding="utf-8") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Title", "Directions", "Quantity","Unit01", "Ingredient01", "Quantity02", "Unit02", "Ingredient02"])
+def getallurl(zoekterm):
+    page = requests.get('https://www.allrecipes.com/search?q='+zoekterm)
+    dehtml = BeautifulSoup(page.content, 'html.parser')
 
-    while True:
-        html = requests.get(url)
-        soup = BeautifulSoup(html.text, 'html.parser')
-        for row in soup.select('table tbody tr'):
-            writer.writerow([c.text if c.text else '' for c in row.select('td')])
+    delijst = dehtml.find(id='search-results__content_1-0')
 
+    #print(delijst.prettify())
+    #print(delijst.find('a')['href'])
+    for card in delijst.find_all('a'):
+        getInfoVanDetail(card['href'])
 
-        if soup.select_one('li.active + li a'):
-            url = soup.select_one('li.active + li a')['href']
-        else:
-            break
+getallurl('cheese')
+print('==============================')
+getallurl('beef')
+url = 'https://www.allrecipes.com/recipe/'
+
+extents = ['20608/super-simple-oven-barbequed-chicken/']
+
+for i in range(0,len(extents)):
+    total_url = url + extents[i]
+    with open('Recipe.csv', "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Title", "Directions", "Quantity","Unit01", "Ingredient01", "Quantity02", "Unit02", "Ingredient02"])
+
+        while True:
+            html = requests.get(total_url)
+            soup = BeautifulSoup(html.content, 'html.parser')
+            for row in soup.find('h1').text:
+                #writer.writerow([c.text if c.text else '' for c in row.find('td')])
+                print(row)
+
+            if soup.select_one('li.active + li a'):
+                total_url = soup.select_one('li.active + li a')['href']
+            else:
+                break
